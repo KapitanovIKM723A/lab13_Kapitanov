9e5cb03cf63bc1d582cdd2a9eba3df069de05e95
Михайло Капітанов <Mykhailo.Kapitanov@infiz.khpi.edu.ua>
2024-04-22 20:25:08 +0300

    Добавьте файлы проекта.

---
+++ lab13_Kapitanov/lab13_Kapitanov.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+int sequence(int n) {
+    if (n == 1) {
+        return 7;
+    }
+    else {
+        return sequence(n - 1) + 10;
+    }
+}
+
+int main() {
+    setlocale(LC_ALL, "ukr");
+    using namespace std;
+    int n;
+    cout << "Введiть номер елемента ряду, щоб отримати його значення: ";
+    cin >> n;
+    if (n < 1 || n > 10) {
+       cout << "Введiть число вiд 1 до 10." << std::endl;
+    }
+    else {
+        cout << "n-й елемент ряду: " << sequence(n) << std::endl;
+    }
+    return 0;
+}
